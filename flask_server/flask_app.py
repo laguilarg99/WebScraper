ddc24c7b5621af0601c405795a86256b41d62228
laguilarg99 <laguilarg99@correo.ugr.es>
2022-02-26 14:15:02 +0100

    #4: All the necessary data fetched from the web

--- flask_server/flask_app.py
+++ flask_server/flask_app.py
@@ -8,13 +8,15 @@ import resources.constants as constants
 
 flask_app = Flask(__name__, template_folder='../web')
 
+main_page = "<br><a href=\"/\">Comeback to main page</a>"
+
 @flask_app.route('/')
 def home():
     return render_template('index.html')
 
 @flask_app.route('/first_filter')
 def firstFilter():
-    return web_scraper.scrape_site(constants.URL)
+    return web_scraper.scrape_site(constants.URL) + main_page
 
 @flask_app.route('/second_filter')
 def secondFilter():
--- scraping_scripts/web_sraping_script.py
+++ scraping_scripts/web_sraping_script.py
@@ -1,6 +1,13 @@
+import sys
+sys.path.append('/app')
+
 import time
+import numpy as np
+import itertools
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import scraping_scripts.models.entry as entry
+
 
 def scrape_site(URL):
     criteria = webdriver.ChromeOptions()
@@ -14,11 +21,23 @@ def scrape_site(URL):
 
     time.sleep(5)
 
-    element1 = driver.find_element_by_class_name('titlelink').text
-    print(element1)
+    titles = driver.find_elements_by_class_name('titlelink')
+    ranks = driver.find_elements_by_class_name('rank')
+    scores = driver.find_elements_by_class_name('score')
+    comments = driver.find_elements_by_partial_link_text('comments')
+
+    #delete first comments link appearance
+    del comments[0]
+
+    final_web = ""
+
+
+    for (title, rank, comment, score) in itertools.zip_longest(titles, ranks, scores, comments):
+        if title is not None and rank is not None and comment is not None and score is not None: 
+            final_web += rank.text + " | " + title.text + " | " + score.text + " | " + comment.text + "<br>"
 
     driver.close()
 
-    return element1
+    return final_web
 
 
